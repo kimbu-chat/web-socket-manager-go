bf9470f5eb6c0a28e38866d206409570a58a13e7
Artem Avanesov <artsyom.avanesov@gmail.com>
2022-05-13 02:02:59 +0400

    change db configs (#28)
    Co-authored-by: Yegor Rybchenko <yegorrybchenko@gmail.com>

--- internal/config/db/config.go
+++ internal/config/db/config.go
@@ -22,7 +22,10 @@ func InitConnection(cfg DbCfg) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", cfg.Host, cfg.User, cfg.Password, cfg.Name, cfg.Port)
 
 	var err error
-	connection, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	connection, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
 	if err != nil {
 		panic(err)
 	}
